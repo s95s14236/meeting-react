d0229ed6f1acc9da05d4d342e1a8d50548343404
Steven Wang <43293917+s95s14236@users.noreply.github.com>
2022-02-14 07:49:27 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC3oqkUEn7KF9wDV2-pPYR0w_-ovrnxPTs",
+//     authDomain: "meeting-6a4a2.firebaseapp.com",
+//     projectId: "meeting-6a4a2",
+//     storageBucket: "meeting-6a4a2.appspot.com",
+//     messagingSenderId: "654889178463",
+//     appId: "1:654889178463:web:9f9de90607b4498fc3d2ea"
+// }
+
+// const app = initializeApp(firebaseConfig);
+
+// export const db = getFirestore(app);
