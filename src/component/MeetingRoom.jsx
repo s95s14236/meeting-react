50fd5b8ce233d31fa66d24a5344b24dc2946ea6d
Steven Wang <s95s14236@gmail.com>
2022-04-17 02:29:35 +0800

    fix: copy url location href

--- src/component/MeetingRoom.jsx
+++ src/component/MeetingRoom.jsx
@@ -172,7 +172,7 @@ const MeetingRoom = () => {
     const joinCall = async () => {
         console.log('joinCall');
         const docRef = doc(db, 'calls', channelIDRef.current)
-        const callDoc = (await getDoc(docRef));
+        const callDoc = await getDoc(docRef);
         const offerCandidatesRef = collection(db, docRef.path, 'offerCandidates');
         const answerCandidatesRef = collection(db, docRef.path, 'answerCandidates');
 
@@ -395,8 +395,8 @@ const MeetingRoom = () => {
      * 複製會議url
      */
     const copyUrl = () => {
-        console.log(`${window.location.href}${channelID}`);
-        navigator.clipboard.writeText(`${window.location.href}${channelID}`);
+        console.log(`${window.location.href}`);
+        navigator.clipboard.writeText(`${window.location.href}`);
     }
 
     /**
@@ -469,7 +469,7 @@ const MeetingRoom = () => {
                         會議代碼：<br />{channelID}<br /><br />
                         會議連結：<br />
                         <div className='flex items-center justify-center w-full'>
-                            <div className=' max-w[85%] text-ellipsis overflow-hidden'>{window.location.href + channelID}</div>
+                            <div className=' max-w[85%] text-ellipsis overflow-hidden'>{window.location.href}</div>
                             <button onClick={copyUrl} className="ml-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
